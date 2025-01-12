af7ae2fe3806fa9cac9670ec88baf4aaeb756b2c
NiHaoKaiLan678 <kl5645525@gmail.com>
2025-01-12 15:33:33 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (0 == receivedNumber) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    basic.clearScreen()
+})
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    radio.sendNumber(1)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    radio.sendNumber(0)
+})
+basic.forever(function () {
+	
+})
